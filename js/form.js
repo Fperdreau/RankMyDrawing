35691d36f4d439509cd0dfd3c277432437e80e5d
Florian Perdreau <florian.perdreau@gmail.com>
2015-09-29 20:59:12 +0200

    added licence terms

--- js/form.js
+++ js/form.js
@@ -5,14 +5,14 @@
  * @copyright Copyright (C) 2014 Florian Perdreau
  * @license <http://www.gnu.org/licenses/agpl-3.0.txt> GNU Affero General Public License v3
  *
- * This file is part of Journal Club Manager.
+ * This file is part of RankMyDrawings.
  *
- * Journal Club Manager is free software: you can redistribute it and/or modify
+ * RankMyDrawings is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Journal Club Manager is distributed in the hope that it will be useful,
+ * RankMyDrawings is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
